eb2c23032215d209a8d3130a05f9f3f2826f6e1e
cjassu <chandan123jaiswal@gmail.com>
2019-08-10 00:47:07 +0530

    Added a Shop page and added the data after goint to style the shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Switch,Route} from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1>Shop Hats</h1>
-  </div>
-);
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
