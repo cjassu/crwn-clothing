98a89c95eeb769a6d76b445ceb84aa40f71dbeae
cjassu <chandan123jaiswal@gmail.com>
2019-08-10 11:48:03 +0530

    updating our app with a shop page , collection preview component and collection item component

--- src/component/directory/directory.component.jsx
+++ src/component/directory/directory.component.jsx
@@ -12,26 +12,26 @@ class Directory extends React.Component{
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'shop'
                 },
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: 'shop'
                 },
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: 'shop'
                 },
                 {
                   title: 'womens',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens',
+                  linkUrl: 'shop',
                   size:'large'
                 },
                 {
@@ -39,7 +39,7 @@ class Directory extends React.Component{
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens',
+                  linkUrl: 'shop',
                   size:'large'
                 }
               ] 
