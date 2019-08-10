cf1153cac608fae426eff7dfac9c6ceb7a82b323
cjassu <chandan123jaiswal@gmail.com>
2019-08-10 12:16:59 +0530

    Added heding component and links to home ,shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './component/header/header.component';
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
