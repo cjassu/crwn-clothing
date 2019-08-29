972f6d69a9f464e5513c0264678de7050d311766
cjassu <chandan123jaiswal@gmail.com>
2019-08-29 23:25:19 +0530

    added add item and remove item functionality in our cart component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,4 +9,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
 
     return[...cartItems,{...cartItemToAdd,quantity:1}]
+};
+
+export const removeItemFromCart = (cartItems,CartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id == CartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity == 1){
+        return cartItems.filter(cartItem => cartItem.id !== CartItemToRemove.id) 
+    }
+
+    return cartItems.map(
+        cartItem => 
+        cartItem.id == CartItemToRemove.id ?
+        {...cartItem, quantity:cartItem.quantity-1} :
+        cartItem
+    )
 }
